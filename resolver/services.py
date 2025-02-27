4d7820593fea8f0d6d328c72af4a6487233b1930
Sina <s.beignazary1383@gmail.com>
2025-02-27 16:12:39 +0330

    Add reverse DNS lookup feature

--- resolver/services.py
+++ resolver/services.py
@@ -1,4 +1,5 @@
 import dns.resolver
+import dns.reversename
 
 def resolve_dns(domain, record_type='A'):
     try:
@@ -24,5 +25,28 @@ def resolve_dns(domain, record_type='A'):
     except dns.resolver.Timeout:
         raise ValueError("DNS query timed out")
 
+
+def reverse_dns(ip_address):
+    try:
+        reverse_name = dns.reversename.from_address(ip_address)
+        reversed_dns = dns.resolver.resolve(reverse_name, 'PTR')
+        for record in reversed_dns:
+            domain_name = []
+            record = str(record)
+            domain_name.append(record)
+
+        return {
+            'ip_address': ip_address,
+            'domain_name': domain_name
+        }
+
+    except dns.resolver.NXDOMAIN:
+       raise ValueError('No domain name associated with this IP address')
+    except dns.resolver.Timeout:
+        raise ValueError('Reverse DNS query timed out.')
+    except dns.resolver.NoNameservers:
+        raise ValueError('No nameservers available to handle this request')
+    except Exception as e:
+        raise ValueError(f'An error occurred: {e}')
 
 
--- resolver/views.py
+++ resolver/views.py
@@ -2,7 +2,7 @@ from django.core.serializers import serialize
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .services import resolve_dns
+from resolver import services
 from resolver import serializers
 
 
@@ -19,13 +19,14 @@ class DnsResolveApiView(APIView):
 
     def post(self, request):
         serializer = serializers.DnsResolveSerializer(data=request.data)
+
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         domain = serializer.validated_data['domain']
         record_type = serializer.validated_data['record_type']
         try:
-            result = resolve_dns(domain, record_type)
+            result = services.resolve_dns(domain, record_type)
             return Response(result)
         except ValueError as e:
             return Response(
@@ -33,3 +34,23 @@ class DnsResolveApiView(APIView):
             )
 
 
+class ReverseDnsResolveApiView(APIView):
+    def get(self, request):
+        serializer = serializers.ReverseDnsResolveSerializer()
+        return Response({
+            'message': 'Enter domain name and record type below.',
+            'form': serializer.data
+        })
+
+    def post(self, request):
+        serializer = serializers.ReverseDnsResolveSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        ip_address = serializer.validated_data['ip_address']
+        try:
+            result = services.reverse_dns(ip_address)
+            return Response(result)
+        except ValueError as e:
+            return Response(
+                {'error': str(e)}, status=status.HTTP_400_BAD_REQUEST
+            )
